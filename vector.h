2a0571ad8cb38beb95a68fb4de8ec0e897d5bb50
You-Ay <38989871+You-Ay@users.noreply.github.com>
2018-05-04 21:29:39 +0200

    vector.h
    3D vectors data structure and basic vector operations.

---
+++ vector.h
@@ -0,0 +1,34 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+// 3D vector data structure
+
+typedef struct {
+	double x, y, z;
+} vec;
+
+vec vec_create(double x, double y, double z); // returns the vector (x,y,z)
+void vec_print(vec v); // prints vector to terminal, e.g. "( 2.1 , 1.0 , 3.1 )"
+
+// Operations
+
+vec add(vec v, vec w); // returns the vector v + w
+vec sub(vec v, vec w); // returns the vector v - w
+
+vec mul(vec v, double a); // scalar multiplication - returns the vector a v
+vec div(vec v, double a); // returns the vector v/a
+
+double dot(vec v, vec w); // inner product, returns the number v . w
+vec out(vec v, vec w); // outer product, returns the vector v x w
+
+// Norms
+
+double norm2(vec v); // returns the square of the Euclidean norm of v
+double norm(vec v); // returns the Euclidean norm |v|
+vec normalize(vec v); // returns the normalized vector v/|v|
+
+#endif
