3cdcadd58975e4f0b9ce7ce478d845a46bd41710
You-Ay <38989871+You-Ay@users.noreply.github.com>
2018-05-04 21:32:51 +0200

    vector.c
    Implementation of the functions specified in the header file.

---
+++ vector.c
@@ -0,0 +1,15 @@
+#include "vector.h"
+
+vec vec_create(double x, double y, double z) {
+	vec result;
+	result.x = x;
+	result.y = y;
+	result.z = z;
+	return result;
+}
+
+void vec_print(vec v) {
+	printf("( %lf , %lf , %lf )", v.x, v.y, v.z)" 
+}
+
+// Implementation of further functions
