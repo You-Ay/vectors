98ef58e122b8a0c54bf250678e4a5eefaf053cb6
Const-He <39199186+Const-He@users.noreply.github.com>
2018-05-16 14:43:38 +0200

    vector.c
    Added basic vector operations and the euclidean norm to vector.c (Issues #1 and #2)

--- vector.c
+++ vector.c
@@ -23,5 +23,87 @@ char * vector_print(vector v, int places) {
 	return result;
 }
 
-// Implementation of further functions
+vector add(vector v, vector w) {
 
+	vector result;
+
+	result.x = v.x + w.x;
+	result.y = v.y + w.y;
+	result.z = v.z + w.z;
+
+	return result;
+
+}
+
+vector subtract(vector v, vector w) {
+
+	vector result;
+
+	result.x = v.x - w.x;
+	result.y = v.y - w.y;
+	result.z = v.z - w.z;
+
+	return result;
+
+}
+
+vector multiply(vector v, double a) {
+
+	vector result;
+
+	result.x = v.x * a;
+	result.y = v.y * a;
+	result.z = v.z * a;
+
+	return result;
+
+}
+
+vector divide(vector v, double a) {
+
+	vector result;
+
+	result.x = v.x / a;
+	result.y = v.y / a;
+	result.z = v.z / a;
+
+	return result;
+
+}
+
+double norm2(vector v) {
+
+	return v.x * v.x + v.y * v.y + v.z * v.z;
+
+}
+
+double norm(vector v) {
+
+	return sqrt(norm2(v));
+
+}
+
+vector normalize(vector v) {
+
+	double length = norm(v);
+
+	if(length != 0) {
+
+		vector result;
+
+		result.x = v.x / length;
+		result.y = v.y / length;
+		result.z = v.z / length;
+
+		return result;
+
+	} else {
+
+		fprintf(stderr, "Error - unable to normalize zero vector! Returned zero vector instead.");
+		return v;
+
+	}
+
+}
+
+// Implementation of further functions
