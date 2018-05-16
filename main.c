951f0a1174ad3a80d972ea4fd4d92d9254fcf6cd
You-Ay <you-ay@pi.com>
2018-05-16 16:44:24 +0200

    Some testing of vector.c routines

--- main.c
+++ main.c
@@ -5,16 +5,20 @@
 // Main function: program starts here
 
 int main() {
+	// Some testing of recently added routines
+
 	vector v = vector_assign(1.0, 2.0, 0.0);
 	vector w = vector_assign(-3.0, 1.0, 1.5);
 
-	point A = vector_assign(1, 1, 1);
-	point B = vector_assign(2, 0, 3);
-	vector u = connect(A, B);
+	printf("    v = %s\n", vector_print(v, 1));
+	printf("    w = %s\n", vector_print(w, 1));
 
-	printf("v = %s\n", vector_print(v, 1));
-	printf("w = %s\n", vector_print(w, 1));
-	printf("u = %s\n", vector_print(u, 1));
+	printf("v + w = %s\n", vector_print(add(v, w), 1));
+	printf("v - w = %s\n", vector_print(subtract(v, w), 1));
+	printf("  2 v = %s\n", vector_print(multiply(v, 2), 1));
+	printf("w / 2 = %s\n", vector_print(divide(w, 2), 2));
+	printf("||v|| = %f\n", norm(v));
+	printf("v_nor = %s\n", vector_print(normalize(v), 3));
 	
 	return 0;
 }
